283de8a359325eb73ab2e8a93a62795a9087d91a
ABLippman <lip@media.mit.edu>
2019-01-26 12:37:26 -0500

    Almost right...   amountField renamed, memo invokes print..  printCheck returns to amountField.  But.  amountField not selected on launch.   viewDidLoad is the wrong place for this

--- CheckWriter/ViewController.swift
+++ CheckWriter/ViewController.swift
@@ -8,15 +8,23 @@
 
 import Cocoa
 
+let today:NSDate = NSDate()
 class ViewController: NSViewController {
-    @IBOutlet weak var input: NSTextField!
+    @IBOutlet weak var amountField: NSTextField!
     @IBOutlet weak var output: NSTextField!
+    @IBOutlet weak var dateField: NSTextFieldCell!
+    @IBOutlet weak var toField: NSTextField!
+    @IBOutlet weak var memoField: NSTextField!
+    @IBOutlet weak var balanceField: NSTextField!
     var moneyMaker: MoneyMaker = MoneyMaker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        setDate()  //And display it in dateField
+        amountField.selectText(Any?.self)
+        self.amountField.becomeFirstResponder()
     }
     
     override var representedObject: Any? {
@@ -25,11 +33,42 @@ class ViewController: NSViewController {
         }
     }
     
-    @IBAction func translate(_ sender: Any) {
-        output.stringValue = moneyMaker.makeMoney(input.stringValue)
-        print(input.stringValue)
-        print (moneyMaker.makeMoney(input.stringValue))
+    @IBAction func amountEntered(_ sender: Any) {
+        output.stringValue = moneyMaker.makeMoney(amountField.stringValue)
+        print("Amount Entered")
+    }
+    
+    @IBAction func toEntered(_ sender: Any) {
+        print("Recipient Entered")
+    }
+    
+    @IBAction func memoEntered(_ sender: Any) {
+        print("Memo Entered")
+    }
+    
+    @IBAction func printCheck(_ sender: Any) {
+        print(amountField.stringValue)
+        print (moneyMaker.makeMoney(amountField.stringValue))
+        // self.becomeFirstResponder()
+        // amountField.selectText(sender)
+        self.amountField.becomeFirstResponder()
+
+    }
+    
+    func setDate() {
+        let dateFormatter = DateFormatter()
+        let shortDate = DateFormatter()
         
+        _ = dateFormatter.dateFormat = "d MMMM, yyyy"
+        _ = shortDate.dateFormat = "d MMM yyyy"
+        
+        
+        let todayString = dateFormatter.string(from: today as Date);
+        let shortString = shortDate.string(from: today as Date)
+        dateField.stringValue = todayString;
+        // registerDate = [shortDate stringFromDate:today];
+        //  Just to see if the short date works...
+        print (shortString)
     }
     
 }
