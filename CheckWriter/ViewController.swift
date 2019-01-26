cfe0cf9e20b2ebdf5d7a714454867ae9d65a9eae
ABLippman <lip@media.mit.edu>
2019-01-26 08:16:37 -0500

    Initial Commit

---
+++ CheckWriter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CheckWriter
+//
+//  Created by lip on 1/26/19.
+//  Copyright © 2019 Andrew Lippman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
