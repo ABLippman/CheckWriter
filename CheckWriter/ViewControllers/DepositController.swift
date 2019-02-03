3b3737bb149a25c4d14d9a9af94244bc2b8181a7
ABLippman <lip@media.mit.edu>
2019-02-02 19:20:25 -0500

    Deposit Window added.  No functionality

---
+++ CheckWriter/ViewControllers/DepositController.swift
@@ -0,0 +1,18 @@
+//
+//  DepositController.swift
+//  CheckWriter
+//
+//  Created by lip on 2/2/19.
+//  Copyright © 2019 Andrew Lippman. All rights reserved.
+//
+
+import Cocoa
+
+class DepositController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
