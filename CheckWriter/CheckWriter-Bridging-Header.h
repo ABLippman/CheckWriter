80575f7993e004f996aaf6f7033700320eadd97f
ABLippman <lip@media.mit.edu>
2019-03-17 13:36:30 -0400

    Extraneous junk gone.  Still a little buggy on creating new accounts.  Now will replace arrays of arrays with structs....

--- CheckWriter/CheckWriter-Bridging-Header.h
+++ CheckWriter/CheckWriter-Bridging-Header.h
@@ -5,4 +5,4 @@
 #import "Money/MoneyMaker.h"
 #import "Money/money.c"
 #import "ObjC/LipScrollView.h"
-#import "Money/NumberTransformer.h"
+
---
+++ CheckWriter/Models/Account.swift
@@ -0,0 +1,9 @@
+//
+//  Account.swift
+//  CheckWriter
+//
+//  Created by lip on 3/17/19.
+//  Copyright © 2019 Andrew Lippman. All rights reserved.
+//
+
+import Foundation
